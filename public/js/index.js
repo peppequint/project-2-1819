73c7047723685c930f8f604dccf6b50988242eae
peppe-quint-boogaard <peppequintb@gmail.com>
2019-04-04 14:15:23 +0200

    installed sw (request failed)

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,12 @@
 console.log("Init");
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(function(register) {
+      console.log("Service worker registrated. Scope is " + register.scope);
+    })
+    .catch(function(error) {
+      console.log("Registration failed with " + error);
+    });
+}
